7a92b198730c54148d86097eac1a2dc53d9e40be
stornquist <stornquist@microsoft.com>
2023-12-13 03:38:43 -0800

    session 3

---
+++ session_3/policy/main.tf
@@ -0,0 +1,74 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+terraform {
+  required_version = ">= 1.0.11"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.84.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "= 3.1.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.7.2"
+    }
+    azurecaf = {
+      source  = "aztfmod/azurecaf"
+      version = "1.2.21"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+#######################################################################################################
+#Custom IL5 Policy
+#######################################################################################################
+
+locals {
+  jsondata    = jsondecode(file("DOD_IL5_audit.json"))
+  parameters  = local.jsondata.properties.parameters
+  policy_list = local.jsondata.properties.policyDefinitions
+}
+
+resource "azurerm_policy_set_definition" "custom_dod_il5_initiative" {
+  name                  = "${var.de_environment}-${var.policy_set_name}"
+  policy_type           = var.policy_type
+  display_name          = "${var.de_environment}-${var.policy_set_name}"
+  description           = var.policy_description
+  parameters            = jsonencode(local.parameters)
+  management_group_id  = var.management_group_id
+
+  metadata = <<METADATA
+    {
+    "category": "Regulatory Compliance",
+    "version":  "19.0.0",
+    "description": "Custom - This initiative includes policies that address a subset of DoD Impact Level 5 (IL5) controls. Additional policies will be added in upcoming releases. For more information, visit https://aka.ms/dodil5-initiative."
+    }
+  METADATA
+  dynamic "policy_definition_reference" {
+    for_each = local.policy_list
+
+    content {
+      policy_definition_id = policy_definition_reference.value["policyDefinitionId"]
+      parameter_values     = try(length(policy_definition_reference.value["parameters"]) > 0, false) ? jsonencode(policy_definition_reference.value["parameters"]) : ""
+      reference_id         = policy_definition_reference.value["policyDefinitionReferenceId"]
+    }
+  }
+}
+resource "azurerm_subscription_policy_assignment" "custom_dod_il5_assignment" {
+  name                 = "${var.de_environment}-${var.policy_set_assignment_name}"
+  display_name         = "${var.de_environment}-${var.policy_set_assignment_name}"
+  policy_definition_id = azurerm_policy_set_definition.custom_dod_il5_initiative.id
+  subscription_id      = var.subscription_id
+  identity {
+    type = "SystemAssigned"
+  }
+  location = "West US"
+}
---
+++ session_3/policy/variables.tf
@@ -0,0 +1,47 @@
+variable "policy_set_name" {
+  type        = string
+  description = "The name of the policy set and assignment."
+  default     = "IL5-Initiative"
+}
+
+variable "policy_type" {
+  type        = string
+  description = "The type of the policy deployment."
+  default     = "Custom"
+}
+
+variable "policy_mode" {
+  type        = string
+  description = "The mode of the policy deployment."
+  default     = "All"
+}
+
+variable "policy_description" {
+  type        = string
+  description = "The description to provide for the policy assignment."
+  default     = "This initiative includes policies that address a subset of DoD Impact Level 5 (IL5) controls. Additional policies will be added in upcoming releases. For more information, visit https://aka.ms/dodil5-initiative."
+}
+
+variable "policy_set_assignment_name" {
+  type        = string
+  description = "The name of the policy set and assignment."
+  default     = "IL5-Assignment"
+}
+
+variable "de_environment" {
+  description = "The Digital Ecosystem environment (dev or prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "subscription_id" {
+  description = "The subscription ID to assign the policy set to."
+  type        = string
+  default = "/subscriptions/c63c958d-d7ac-46a6-a4ed-aabf5adbd706"
+}
+
+variable "management_group_id" {
+  description = "The management group ID to assign the policy set to."
+  type        = string
+  default     = "/managmentgroups/242da254-1fea-0364-2584-c930f11f89c2"
+}
