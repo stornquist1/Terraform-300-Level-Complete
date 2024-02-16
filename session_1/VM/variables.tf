7eb8ccc651720797803d6f4bc86006d9793e6109
jamsmit <jamsmit@microsoft.com>
2024-02-16 12:00:37 -0500

    vm code

---
+++ session_1/VM/variables.tf
@@ -0,0 +1,16 @@
+provider "azurerm" {
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  features {}
+}
+
+variable "subscription_id" {
+}
+variable "client_id" {
+}
+variable "client_secret" {
+}
+variable "tenant_id" {
+}
